4bc5de8472fb8b311cee0bb00631267aa8b54e09
Enrique Coscarelli <enriquecoscarelli@Enriques-MacBook-Air.local>
2022-01-17 07:10:56 -0300

    First Commit

---
+++ domexample.js
@@ -0,0 +1,29 @@
+document.getElementById("main-header").style.background = "red";
+document.getElementById("main-header").style.height = "100px";
+document.getElementById("main-header").innerHTML = "Hello Guillermo! Do you play bass?";
+
+const allPTag = document.getElementsByTagName("p")[0].style.background = "green"
+console.log(allPTag)
+
+document.getElementsByClassName("subheader")[1].innerHTML = "It's raining in Argentina"
+
+document.body.onload = addElement;
+
+function addElement () {
+  // create a new div element
+  const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode("Hi there and greetings!");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+}
+
+const alerter = () => alert("BOOM!")
+
+document.getElementById("main-header").addEventListener('click', alerter)
